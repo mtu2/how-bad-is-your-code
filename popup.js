e83986cc8a470a3f9c0e2002abc59cb75564ac89
Michael <michael52423@gmail.com>
2021-01-29 10:38:26 +1100

    Search now pulls from sites listed in options

--- popup.js
+++ popup.js
@@ -4,49 +4,74 @@ function buildPopupDom() {
   chrome.storage.sync.get(
     { sites: "https://stackoverflow.com/", countDaily: false },
     (items) => {
-      console.log(items);
+      const urls = items.sites.split(/\r?\n/);
+      getVisitTimes(urls);
     }
   );
 }
 
 // Search history to find number of vists to specified url
-async function getVisitTimes(url) {
+function getVisitTimes(urls) {
   const microsecondsForFiveYears = 1000 * 60 * 60 * 24 * 365 * 5;
   const fiveYearsAgo = new Date().getTime() - microsecondsForFiveYears;
+  const urlVisitTimes = {};
+  let checkedUrls = 0;
+  let lastActive = new Date("January 1, 1970");
+  let totalVisits = 0;
 
-  chrome.history.search(
-    {
-      text: url,
-      startTime: fiveYearsAgo,
-      maxResults: 1000000000,
-    },
-    (historyItems) => {
-      const visitTimes = historyItems.map((item) => item.lastVisitTime);
-      processVisitTimes(visitTimes);
-      setLastActive(visitTimes[0]);
-      setTotalVisits(visitTimes.length);
-    }
-  );
+  for (const url of urls) {
+    chrome.history.search(
+      {
+        text: url,
+        startTime: fiveYearsAgo,
+        maxResults: 1000000000,
+      },
+      (historyItems) => {
+        const visitTimes = historyItems.map((item) => item.lastVisitTime);
+        urlVisitTimes[url] = visitTimes;
+
+        // check and update last active and total visits and increment checked urls
+        if (new Date(visitTimes[0]) > lastActive) {
+          lastActive = new Date(visitTimes[0]);
+        }
+        totalVisits += visitTimes.length;
+        checkedUrls++;
+
+        // if all urls checked then process
+        if (checkedUrls == urls.length) {
+          processVisitTimes(urlVisitTimes);
+          setLastActive(lastActive);
+          setTotalVisits(totalVisits);
+        }
+      }
+    );
+  }
 }
 
 // Sort visits into map with key: date and value: number of visits
-function processVisitTimes(visitTimes) {
-  const timeSummary = new Map();
-  visitTimes.forEach((visitTime) => {
-    const dateObj = new Date(visitTime);
-    const dateStr = `${dateObj.getDate()}${
-      dateObj.getMonth() < 10 ? "0" + dateObj.getMonth() : dateObj.getMonth()
-    }${dateObj.getFullYear()}`;
+function processVisitTimes(urlVisitTimes) {
+  const urlTimeSummary = {};
 
-    if (timeSummary.has(dateStr)) {
-      timeSummary.set(dateStr, timeSummary.get(dateStr) + 1);
-    } else {
-      timeSummary.set(dateStr, 1);
-    }
-  });
+  // Enter key of url and value of time summary map
+  for (const url in urlVisitTimes) {
+    const timeSummary = new Map();
 
-  console.log(timeSummary);
-  return timeSummary;
+    urlVisitTimes[url].forEach((time) => {
+      const dateObj = new Date(time);
+      const dateStr = `${dateObj.getDate()}${
+        dateObj.getMonth() < 10 ? "0" + dateObj.getMonth() : dateObj.getMonth()
+      }${dateObj.getFullYear()}`;
+
+      if (timeSummary.has(dateStr)) {
+        timeSummary.set(dateStr, timeSummary.get(dateStr) + 1);
+      } else {
+        timeSummary.set(dateStr, 1);
+      }
+    });
+    urlTimeSummary[url] = timeSummary;
+  }
+
+  console.log(urlTimeSummary);
 }
 
 // Set "Last Active" to last time url visited
